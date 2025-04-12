7f66905e3dd8865c8f89753a604955486da1d96d
Michal Voscek <michal.voscek@gmail.com>
2025-04-12 19:35:56 +0200

    add copy relative path shortcut

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+function insertFullPath()
+  local filepath = vim.fn.expand("%:.")
+  vim.fn.setreg("+", filepath) -- write to clippoard
+end
+vim.keymap.set("n", "<leader>pc", insertFullPath, { noremap = true, silent = true, desc = "Yank Relative Path" })
